e3cb45a989cbab14b322fa1081c2062378aacbc3
Maycol1414Cp <Maycol1414Cp@gmail.com>
2026-05-21 23:17:12 -0400

    feat: add edit and get products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,8 @@
 import pool from "../config/database.js";
 
+ 
+
+//POST /api/products 
 export const createProduct = async (req, res) => {
      
     const { 
@@ -38,6 +41,64 @@ export const createProduct = async (req, res) => {
     } catch (error) {
         console.error(':( Error al crear producto:\n', error.message);
         res.status(500).json({ error: "Error al crear producto" });
+        // El código "23505" es el estándar de Postgres para "Unique Violation"
+        if (error.code === '23505') {
+        return res.status(400).json({ 
+            error: "El SKU ya existe", 
+            message: `Ya hay un repuesto registrado con el código ${sku}.` 
+        });
+        }
     }
+}
 
+//GET /api/products
+export const getProducts = async (req, res) => {
+    try{
+        const queryText = `SELECT * FROM truck_parts;`;
+
+        const result = await pool.query(queryText);
+
+        res.status(200).json({
+            message: "Productos obtenidos exitosamente",
+            products: result.rows
+        });
+    } catch (error) {
+        console.error(':( Error al obtener productos:\n', error.message);
+        res.status(500).json({ error: "Error al obtener productos" });
+    }
+}
+
+//PATCH /api/products
+export const editProduct = async (req, res) => {
+    const { id } = req.params;
+    const {part_name, brand, sku, model_year, stock, price, category_id } = req.body;
+    try {
+        const queryText = `
+            UPDATE truck_parts 
+            SET 
+                part_name = $1, 
+                brand = $2, 
+                sku = $3, 
+                model_year = $4, 
+                stock = $5, 
+                price = $6, 
+                category_id = $7,
+                updated_at = CURRENT_TIMESTAMP
+            WHERE id = $8
+            RETURNING *;
+        `
+        const values = [part_name, brand, sku, model_year, stock, price, category_id, id]
+        const response = await pool.query(queryText, values);
+        
+        if (response.rowCount === 0) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+        res.status(200).json({
+            message: "Producto actualizado exitosamente",
+            product: response.rows[0]
+        });
+    }catch (error) {
+        console.error(':( Error al actualizar producto:\n', error.message);
+        res.status(500).json({ error: "Error al actualizar producto" });
+    }
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { createProduct } from '../controllers/product.controller.js';
+import { createProduct,getProducts } from '../controllers/product.controller.js';
 
 const router = Router();
 
 //endpoint POST /api/products
 router.post('/', createProduct);
+//encdpoint GET /api/products
+router.get('/', getProducts);
 
 export default router;
